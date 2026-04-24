08957e5fb5c85b8bc136c61161b919723afcab2a
Jafar588 <167721983+Jafar588@users.noreply.github.com>
2026-04-24 14:01:39 +0300

    Update bot.py

--- bot/bot.py
+++ bot/bot.py
@@ -1,6 +1,7 @@
 import sys
 import os
-
+sys.path.append(os.getcwd())
+from config import TOKEN
 # إضافة المجلد الرئيسي للمشروع إلى مسارات بايثون
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
