63c87355680faab135bda805779cc3569e7174bc
Jafar588 <167721983+Jafar588@users.noreply.github.com>
2026-04-24 13:15:43 +0300

    Update downloader.py

--- core/downloader.py
+++ core/downloader.py
@@ -1,6 +1,8 @@
 import yt_dlp
 import asyncio
-from config import BASE_OPTS
+import os
+from config import BASE_OPTS, TEMP_PATH
+from core.progress import hook
 
 async def extract(url):
     loop = asyncio.get_event_loop()
@@ -12,10 +14,14 @@ async def extract(url):
     return await loop.run_in_executor(None, run)
 
 
-async def download_mp3(url, path):
+async def download_mp3(url):
+    filename = os.path.join(TEMP_PATH, "%(title)s.%(ext)s")
+
     opts = {
         **BASE_OPTS,
-        "outtmpl": path,
+        "format": "bestaudio/best",
+        "outtmpl": filename,
+        "progress_hooks": [hook],
         "postprocessors": [{
             'key': 'FFmpegExtractAudio',
             'preferredcodec': 'mp3',
@@ -29,3 +35,24 @@ async def download_mp3(url, path):
             ydl.download([url])
 
     await loop.run_in_executor(None, run)
+
+    return filename.replace("%(title)s.%(ext)s", "")
+
+
+async def download_video(url):
+    filename = os.path.join(TEMP_PATH, "%(title)s.%(ext)s")
+
+    opts = {
+        **BASE_OPTS,
+        "outtmpl": filename
+    }
+
+    loop = asyncio.get_event_loop()
+
+    def run():
+        with yt_dlp.YoutubeDL(opts) as ydl:
+            ydl.download([url])
+
+    await loop.run_in_executor(None, run)
+
+    return filename
