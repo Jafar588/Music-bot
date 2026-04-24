5453b45496016db798e4c8bdaff51edb7f4483ee
Jafar588 <167721983+Jafar588@users.noreply.github.com>
2026-04-24 20:59:01 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,12 @@
 import os
 
+# التوكن من السيرفر
 TOKEN = os.getenv("BOT_TOKEN")
+
+# يوزر المجموعة (ميزة الاشتراك الإجباري في الخاص)
 FORCE_SUB_CHANNEL = "@cdzo1song"
 
+# إعدادات البحث السريع (جلب 10 أسماء)
 YDL_SEARCH_OPTIONS = {
     'format': 'bestaudio/best',
     'quiet': True,
@@ -11,7 +15,7 @@ YDL_SEARCH_OPTIONS = {
     'ignoreerrors': True,
 }
 
-# --- (الكود 7: استخدام محرك Aria2c الصاروخي) ---
+# إعدادات التحميل الصاروخية (باستخدام محرك aria2c)
 YDL_DOWNLOAD_OPTIONS = {
     'format': 'm4a/bestaudio[ext=m4a]/bestaudio', 
     'quiet': True,
@@ -21,8 +25,7 @@ YDL_DOWNLOAD_OPTIONS = {
     'outtmpl': 'temp/%(title)s.%(ext)s', 
     'user_agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
     
-    # تفعيل المحرك الخارجي
+    # تفعيل محرك التحميل الخارجي لكسر قيود السيرفر
     'external_downloader': 'aria2c',
-    # إجبار المحرك على فتح 16 اتصال لتقطيع الملف وسحبه بثواني
     'external_downloader_args': ['-x', '16', '-s', '16', '-k', '1M']
 }
