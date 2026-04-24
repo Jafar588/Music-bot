f40c9edc86e39061e51a80c50e39c8300f2f671d
Jafar588 <167721983+Jafar588@users.noreply.github.com>
2026-04-24 14:24:54 +0300

    Update db.py

--- database/db.py
+++ database/db.py
@@ -1,16 +1,3 @@
-import sqlite3
-
-conn = sqlite3.connect("data.db", check_same_thread=False)
-cur = conn.cursor()
-
-cur.execute("""
-CREATE TABLE IF NOT EXISTS fav(
-user_id INTEGER,
-title TEXT,
-url TEXT
-)
-""")
-
-def add_fav(user, title, url):
-    cur.execute("INSERT INTO fav VALUES (?,?,?)", (user, title, url))
-    conn.commit()
+# هنا سيتم بناء نظام المفضلات مستقبلاً
+def add_fav(user_id, data):
+    pass
