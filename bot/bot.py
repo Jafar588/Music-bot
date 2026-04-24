add6db20742594b01ab5d2824106b640fc6c5c78
Jafar588 <167721983+Jafar588@users.noreply.github.com>
2026-04-24 23:55:08 +0300

    Update bot.py

--- bot/bot.py
+++ bot/bot.py
@@ -29,7 +29,7 @@ def home():
     return "Bot is Alive and Running Perfect!"
 
 def run_web_server():
-    port = int(os.environ.get("PORT", 8080))
+    port = int(os.environ.get("PORT", 7860))
     app.run(host="0.0.0.0", port=port)
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
