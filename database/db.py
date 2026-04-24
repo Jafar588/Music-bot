1fe4531e07afd30fd765150857e27f333925011d
Jafar588 <167721983+Jafar588@users.noreply.github.com>
2026-04-24 13:16:06 +0300

    Update db.py

--- database/db.py
+++ database/db.py
@@ -1,19 +1,3 @@
-import sqlite3
-
-conn = sqlite3.connect("data.db", check_same_thread=False)
-cur = conn.cursor()
-
-cur.execute("""
-CREATE TABLE IF NOT EXISTS fav(
-user_id INTEGER,
-title TEXT,
-url TEXT
-)
-""")
-
-def add_fav(user, title, url):
-    cur.execute("INSERT INTO fav VALUES (?,?,?)", (user, title, url))
+def remove_fav(user, url):
+    cur.execute("DELETE FROM fav WHERE user_id=? AND url=?", (user, url))
     conn.commit()
-
-def get_fav(user):
-    return cur.execute("SELECT title,url FROM fav WHERE user_id=?", (user,)).fetchall()
